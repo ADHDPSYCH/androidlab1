a18e980f837cf4c10d15ca6f9285a15149f3a56d
ADHDPSYCH <h417rtc@gmail.com>
2024-11-21 01:38:21 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/eloel111112xdxd/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.eloel111112xdxd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.eloel111112xdxd.ui.theme.ELOEL111112XDXDTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ELOEL111112XDXDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Surface(color = Color.Blue) {
+        Text(
+            text = "hI mY NAMe IS anDdRoiD!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = false)
+@Composable
+fun GreetingPreview() {
+    ELOEL111112XDXDTheme {
+        Greeting("Android")
+    }
+}
